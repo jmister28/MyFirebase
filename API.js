299d197f64172199cb78d7d8d5d33c38ddcde8f3
jmister28 <jmister28@users.noreply.github.com>
2015-02-23 16:25:54 -0800

    Update API.js

--- API.js
+++ API.js
@@ -1,1 +1,15 @@
-
+var setup = function(URL) {
+  var myFire = new Firebase (URL);
+}
+var writeNormal = function(data) {
+  myFire.set(data);
+}
+var writeList = function(listData) {
+  myFire.push(listData);
+}
+var view = function() {
+  myFire.on("child_added", function(snapshot) {
+    var f = writeNormal.val();
+    document.getElementById("#").innerHTML = f;
+  }
+}
